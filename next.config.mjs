51d7ef3dcb858888071300b8477ebb5acfc06225
Satyam Jain <satyam26092004@gmail.com>
2025-11-29 21:26:42 +0530

    update commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { appDir: true },
-  srcDir: 'src'
+  srcDir: 'src',
+  images: {
+  domains: ['chatsecretbackend.onrender.com'],
+}
+
 };
 
 export default nextConfig;
