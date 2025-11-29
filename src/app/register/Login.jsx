9593a39f65a2e3e4caed6f3d2e58aafcfc216265
Satyam Jain <satyam26092004@gmail.com>
2025-11-30 00:40:39 +0530

    update socket commit

--- src/app/register/Login.jsx
+++ src/app/register/Login.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState ,useEffect} from "react";
 import {motion} from "framer-motion"
 import { io } from "socket.io-client";
 import { useRouter } from "next/router";
