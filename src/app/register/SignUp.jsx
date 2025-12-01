ff3c7bf1c483cd6dd42597131bbac36517a4cf4b
Satyam Jain <satyam26092004@gmail.com>
2025-12-01 15:47:19 +0530

    sign up push

--- src/app/register/SignUp.jsx
+++ src/app/register/SignUp.jsx
@@ -57,18 +57,18 @@ export default function Signupuser(){
     <div className="min-h-screen w-full flex bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 px-4 py-6">
 
       {/* LEFT SIDE – Illustration / Dummy Chat */}
-      <div className="hidden md:flex flex-col justify-center w-1/2 px-10">
+      <div className="absolute inset-0 z-0  flex flex-col justify-center md:static md:z-auto md:w-1/2  w-1/2 px-10">
         
-        <h1 className="text-4xl font-bold text-white leading-tight mb-4">
+        <h1 className="text-4xl md:flex hidden font-bold text-white leading-tight mb-4">
           Create Your Account  
         </h1>
 
-        <p className="text-gray-300 text-lg mb-10">
+        <p className="text-gray-300 md:flex hidden text-lg mb-10">
           Join the community. Connect, chat and explore new possibilities.
         </p>
 
         {/* Chat Bubbles Illustration */}
-        <div className="space-y-4">
+        <div className="space-y-10  md:space-y-4">
           
           <div className="flex gap-3">
             <div className="w-10 h-10 rounded-full bg-gray-700"></div>
@@ -84,41 +84,79 @@ export default function Signupuser(){
             <div className="w-10 h-10 rounded-full bg-gray-700"></div>
           </div>
 
-          <div className="flex gap-3">
+          <div className="flex gap-3 ml-30">
             <div className="w-10 h-10 rounded-full bg-gray-700"></div>
             <div className="bg-gray-700 px-4 py-3 rounded-2xl shadow-lg w-52">
               <div className="w-28 h-3 bg-gray-600 rounded-md"></div>
             </div>
           </div>
 
+           <div className="flex gap-3 justify-end  ">
+            <div className="bg-gray-700 px-4 py-3 rounded-2xl shadow-lg w-48">
+              <div className="w-32 h-3 bg-gray-600 rounded-md"></div>
+            </div>
+            <div className="w-10 h-10 rounded-full bg-gray-700"></div>
+          </div>
+          <div className="flex gap-3 justify-start">
+            <div className="bg-gray-700 px-4 py-3 rounded-2xl shadow-lg w-48">
+              <div className="w-32 h-3 bg-gray-600 rounded-md"></div>
+            </div>
+            <div className="w-10 h-10 rounded-full bg-gray-700"></div>
+          </div>
+
+<div className="flex md:hidden gap-3 justify-center ml-30">
+            <div className="bg-gray-700 px-4 py-3 rounded-2xl shadow-lg w-48">
+              <div className="w-32 h-3 bg-gray-600 rounded-md"></div>
+            </div>
+            <div className="w-10 h-10 rounded-full bg-gray-700"></div>
+          </div>
+          <div className="flex md:hidden gap-3 justify-center ml-30">
+            <div className="bg-gray-700 px-4 py-3 rounded-2xl shadow-lg w-48">
+              <div className="w-32 h-3 bg-gray-600 rounded-md"></div>
+            </div>
+            <div className="w-10 h-10 rounded-full bg-gray-700"></div>
+          </div>
+<div className="flex md:hidden gap-3 justify-end ml-30">
+            <div className="bg-gray-700 px-4 py-3 rounded-2xl shadow-lg w-48">
+              <div className="w-32 h-3 bg-gray-600 rounded-md"></div>
+            </div>
+            <div className="w-10 h-10 rounded-full bg-gray-700"></div>
+          </div>
+          <div className="flex md:hidden gap-3 justify-start ml-30">
+            <div className="bg-gray-700 px-4 py-3 rounded-2xl shadow-lg w-48">
+              <div className="w-32 h-3 bg-gray-600 rounded-md"></div>
+            </div>
+            <div className="w-10 h-10 rounded-full bg-gray-700"></div>
+          </div>
+
         </div>
       </div>
 
       {/* RIGHT SIDE – Signup Form */}
-      <div className="flex justify-center pt-10  items-center w-full md:w-1/2">
-        <div className="w-full max-w-md bg-white/10 backdrop-blur-xl text-white p-8  rounded-2xl shadow-xl space-y-6 border border-white/10">
+      <div className="flex justify-center md:pt-10  items-center w-full md:w-1/2">
+        <div className="w-full max-w-md bg-white/10 backdrop-blur-[2px] text-white p-8  rounded-2xl shadow-xl space-y-6 border border-white/10">
 
           <h2 className="text-3xl font-semibold text-center">Sign Up</h2>
           <form onSubmit={handlesubmit} className="h-full w-full">
 
           <div className="space-y-3">
             <input type="text" placeholder="Username" onChange={handleChanges} value={formdata.username} name="username"
-              className="w-full px-2 py-2 bg-white/20 border border-white/20 rounded-xl focus:border-emerald-400 outline-none"/>
+              className="w-full px-4 py-2 sm:px-6 bg-white/20 border border-white/20 rounded-xl focus:border-emerald-400 outline-none"/>
             
             <input type="email" placeholder="Email" onChange={handleChanges} value={formdata.email} name="email"
-              className="w-full px-2 py-2 bg-white/20 border border-white/20 rounded-xl focus:border-emerald-400 outline-none"/>
+              className="w-full px-4 py-2 sm:px-6 bg-white/20 border border-white/20 rounded-xl focus:border-emerald-400 outline-none"/>
 
             <input type="password" placeholder="Password" onChange={handleChanges} value={formdata.password} name="password"
-              className="w-full px-2 py-2 bg-white/20 border border-white/20 rounded-xl focus:border-emerald-400 outline-none"/>
+              className="w-full px-4 py-2 sm:px-6 bg-white/20 border border-white/20 rounded-xl focus:border-emerald-400 outline-none"/>
 
             <input type="number" placeholder="Phone Number" onChange={handleChanges} value={formdata.phonenumber} name="phonenumber"
-              className="w-full px-2 py-2 bg-white/20 border border-white/20 rounded-xl focus:border-emerald-400 outline-none"/>
+              className="w-full px-4 py-2 sm:px-6 bg-white/20 border border-white/20 rounded-xl focus:border-emerald-400 outline-none"/>
 
             <input type="text" placeholder="Full Name" onChange={handleChanges} value={formdata.name} name="name"
-              className="w-full px-2 py-2 bg-white/20 border border-white/20 rounded-xl focus:border-emerald-400 outline-none"/>
+              className="w-full px-4 py-2 sm:px-6 bg-white/20 border border-white/20 rounded-xl focus:border-emerald-400 outline-none"/>
 
-            <input type="file" onChange={handleChanges} name="avatar" accept="/*"
-              className="w-full p-3 bg-white/20 border border-white/20 rounded-xl cursor-pointer"/>
+            <input type="file" onChange={handleChanges} name="avatar" accept="image/*"
+              className="w-full px-4 py-2 sm:px-6 bg-white/20 border border-white/20 rounded-xl cursor-pointer"/>
           
           
 
