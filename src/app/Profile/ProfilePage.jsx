5e078dcd60e82fa6a452bc3c29751af0dfe1990b
Satyam Jain <satyam26092004@gmail.com>
2025-12-04 01:05:50 +0530

    fix logout loader

--- src/app/Profile/ProfilePage.jsx
+++ src/app/Profile/ProfilePage.jsx
@@ -17,6 +17,7 @@ export default function ProfilePage() {
   const navigtor = useRouter()
   const [message , setmessage] = useState("")
   const [loader , setloader] = useState(false)
+  const [logoutloader , setlogoutloader] = useState(false)
 
 
 
@@ -87,13 +88,13 @@ const handlesubmit = async(e) => {
 }
 const handleLogout = async() => {
         try {
-          setloader(true)
+          setlogoutloader(true)
            await axios.post("https://chatsecretbackend.onrender.com/api/loggout", {}, { withCredentials:true })
 
             localStorage.removeItem("token")
             localStorage.removeItem("username")
             localStorage.removeItem("welcomeShown")
-            setloader(false)
+            setlogoutloader(false)
 
             window.location.href="/register"
             
@@ -204,7 +205,7 @@ const handleLogout = async() => {
             </div>
             <div className="flex flex-col gap-5">
               <p className="text-white text-xl">Log out your Account</p>
-            <button onClick={handleLogout} className="px-7 py-3  bg-white text-black text-center rounded-2xl hover:bg-white/80">{loader ? <Loader2 className="h-7 w-7 animate-spin"/>:"LogOut"}</button>
+            <button onClick={handleLogout} className="px-7 py-3  bg-white text-black text-center rounded-2xl hover:bg-white/80">{logoutloader ? <Loader2 className="h-7 w-7 animate-spin"/>:"LogOut"}</button>
             </div>
 
           </div>
