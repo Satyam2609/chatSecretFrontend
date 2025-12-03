cb098f304f8513cb54aa3973fee1213a9a105d0b
Satyam Jain <satyam26092004@gmail.com>
2025-12-04 01:35:34 +0530

    fix message

--- src/app/Group/ChartAndtalk.jsx
+++ src/app/Group/ChartAndtalk.jsx
@@ -170,7 +170,7 @@ export default function ChartAndtalk() {
           absolute md:static h-screen md:h-auto transition-all duration-300`}
       >
         {/* Header */}
-        <div className="flex justify-between items-center bg-black text-white p-3 rounded-xl mb-2">
+        <div className="flex justify-between md:mt-15 mt-0 items-center bg-black text-white p-3 rounded-xl mb-2">
           <div className="md:hidden cursor-pointer" onClick={() => setShowRightPanel(false)}>Back</div>
           <span>{chosenRoom}</span>
           <span className="cursor-pointer" onMouseEnter={() => setShowMembers(true)}>Members</span>
